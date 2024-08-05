88f7fd80c77538c08a2c0ab496428eb1f057d3e1
tanvir <shkhtanvirahmed@gmail.com>
2024-08-05 21:00:05 +0600

    Automated commit message

--- cf/all 800/A_SpellCheck.cpp
+++ cf/all 800/A_SpellCheck.cpp
@@ -13,33 +13,44 @@ int ans[26] = {0};
 void solve(){
     int n; cin >> n;
     string st; cin >> st;
-    hi;
-    if(n > str.size() || n < str.size()){
+    if(n != str.size()){
+        // cout << "length" << endl;
         cout << "NO" << endl;
         return;
     }
-    if(find(st.begin(), st.end(), 'T') == st.end()){
+    auto it = find(st.begin(), st.end(), 'T');
+    if(it == st.end()){
+        // cout << "no T" << endl;
         cout << "NO" << endl;
         return;
     }
-    int idx = find(st.begin(), st.end(), 'T')-st.begin();
-    //pr(idx);
-    st.erase(idx, 1);
+
+    // pr(it-st.begin());
+    st.erase(it-st.begin(), 1);
+    // pr(st);
+
+    //making the ans[], zero if they're not
+    for(int i=0; i<26; i++) ans[i] = 0;
 
     for(int i=0; i<st.size(); i++){
-        if(st[i] >= 'a' && st[i] <= 'z') ans[st[i]-'a']++;
+        if(st[i] >= 'a' && st[i] <= 'z'){
+            ans[st[i]-'a']++;
+        }
         else{
+            // cout << "Another Cap" << endl;
             cout << "NO" << endl;
             return;
         }
     }
 
     for(int i=0; i<26; i++){
-        if(ans[i] != mat[i]){
+        if(mat[i] != ans[i]){
+            // cout << "didn't match" << endl;
             cout << "NO" << endl;
             return;
         }
     }
+
     cout << "YES" << endl;
 }
 
@@ -50,6 +61,7 @@ int main(){
     for(int i=1; i<str.size(); i++) mat[str[i]-'a']++;
 
     int t; cin >> t;
+    // int t = 1;
     while(t--) solve();
 
     return 0;
