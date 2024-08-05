fca0c065004d12b218db312c78c40e7b17b1f7d8
tanvir <shkhtanvirahmed@gmail.com>
2024-08-05 23:00:04 +0600

    Automated commit message

---
+++ cf/all 800/A_VanyaAndCubes03.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define input freopen("in.txt", "r", stdin)
+#define pr(x) cout << #x << " = " << x << endl
+#define prt(x) cout << x << endl
+#define endl "\n"
+#define hi cout << "hi" << endl
+//#define int long long
+//#define int unsigned long long
+
+void solve(){
+    int n; cin >> n;
+    int h = 1;
+    int cal = ((h+1)*(h+2)*h)/6;
+
+    while(cal <= n){
+        h++;
+        cal = ((h+1)*(h+2)*h)/6;
+    }
+
+    prt(--h);
+}
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    //int t; cin >> t;
+    int t = 1;
+    while(t--) solve();
+
+    return 0;
+}
