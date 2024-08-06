d282abcdd18568c714f6a8e453f56671587dc4c5
tanvir <shkhtanvirahmed@gmail.com>
2024-08-06 22:00:06 +0600

    Automated commit message

---
+++ cf/all 800/div4/B_CardGame.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define input freopen("in.txt", "r", stdin)
+#define pr(x) cout << #x << " = " << x << endl
+#define prt(x) cout << x << endl
+#define endl "\n"
+#define hi cout << "hi" << endl
+//#define int long long
+//#define int unsigned long long
+
+void solve(){
+    int a1, a2, b1, b2; cin >> a1 >> a2 >> b1 >> b2;
+
+    //if all nums are same
+    if(a1 == a2 && a2 == b1 && b1 == b2){
+        prt(0);
+        return;
+    }
+
+    //if each persons 2 cards are same
+    if(a1 == a2 && b1 == b2){
+        if(a1 > b1){
+            prt(4);
+            return;
+        }
+        else if(b1 > a1){
+            prt(0);
+            return;
+        }
+    }
+
+    //a1, a2 > b1, b2
+    if(max(a1, a2) > max(b1, b2) && min(a1, a2) > max(b1, b2)){
+        prt(4);
+        return;
+    }
+    else if(max(b1, b2) > max(a1, a2) && min(b1, b2) > max(a1, a2)){
+        prt(0);
+        return;
+    }
+
+    if(max(a1, a2) < max(b1, b2) && min(a1, a2) > min(b1, b2)){
+        
+    }
+}
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    //int t; cin >> t;
+    int t = 1;
+    while(t--) solve();
+
+    return 0;
+}
---
+++ cf/all 800/div4/E_tripleOperations.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define input freopen("in.txt", "r", stdin)
+#define pr(x) cout << #x << " = " << x << endl
+#define prt(x) cout << x << endl
+#define endl "\n"
+#define hi cout << "hi" << endl
+//#define int long long
+//#define int unsigned long long
+
+void solve(){
+    int l, r; cin >> l >> r;
+    set<int> st;
+    for(int i=l; i<=r; i++) st.insert(i);
+
+    for(auto X:st) cout << X << " ";
+    cout << endl;
+}
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    //int t; cin >> t;
+    int t = 1;
+    while(t--) solve();
+
+    return 0;
+}
