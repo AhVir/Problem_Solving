596579be759720810f38c00bfde7ca8df5ab39f5
tanvir <shkhtanvirahmed@gmail.com>
2024-08-07 06:00:06 +0600

    Automated commit message

---
+++ cf/all 800/A_CreatingWord.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define input freopen("in.txt", "r", stdin)
+#define pr(x) cout << #x << " = " << x << endl
+#define prt(x) cout << x << endl
+#define endl "\n"
+#define hi cout << "hi" << endl
+//#define int long long
+//#define int unsigned long long
+
+void solve(){
+    string st1, st2; cin >> st1 >> st2;
+    char t = st1[0];
+    st1[0] = st2[0];
+    st2[0] = t;
+    
+    cout << st1 << " " << st2 << endl;
+}
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t; cin >> t;
+    // int t = 1;
+    while(t--) solve();
+
+    return 0;
+}
---
+++ cf/all 800/A_DivisibilityProblem.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define input freopen("in.txt", "r", stdin)
+#define pr(x) cout << #x << " = " << x << endl
+#define prt(x) cout << x << endl
+#define endl "\n"
+#define hi cout << "hi" << endl
+//#define int long long
+//#define int unsigned long long
+
+void solve(){
+    int a, b; cin >> a >> b;
+    if(a%b == 0) prt(0);
+    else{
+        int cal = a/b;
+        cal++;
+        cal *= b;
+        prt(cal-a);
+    }
+}
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    //int t; cin >> t;
+    int t = 1;
+    while(t--) solve();
+
+    return 0;
+}
