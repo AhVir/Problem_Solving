b4dcf047320fad8d894800fa81db0813c3702b86
tanvir <shkhtanvirahmed@gmail.com>
2024-08-07 09:00:05 +0600

    Automated commit message

--- cf/all 900/B_makeMajority.cpp
+++ cf/all 900/B_makeMajority.cpp
@@ -6,40 +6,34 @@ using namespace std;
 #define prt(x) cout << x << endl
 #define endl "\n"
 #define hi cout << "hi" << endl
-//#define int long long
+#define int long long
 //#define int unsigned long long
 
 void solve(){
     int n; cin >> n;
-    vector<int> nums;
+    string nums; cin >> nums;
+
+    for(int i=0; i<n-1; i++){
+        if(nums[i] == '0' && nums[i+1] != '1') nums[i] = '_';
+    }
+
+    // pr(nums);
+    int cnt0 = 0, cnt1 = 0;
     for(int i=0; i<n; i++){
-        int t; cin >> t;
-        nums.push_back(t);
+        if(nums[i] == '0') cnt0++;
+        else if(nums[i] == '1') cnt1++;
     }
 
-    //if all are 0s
-    int cnt0 = count(nums.begin(), nums.end(), 0);
-    if(cnt0 == n){
-        prt("No");
-        return;
-    }
-
-    //if all are 1s
-    int cnt1 = count(nums.begin(), nums.end(), 1);
-    if(cnt1 == n || cnt1 > cnt0){
-        prt("YES");
-        return;
-    }
-
-    
+    if(cnt1>cnt0) prt("Yes");
+    else prt("NO");
 }
 
 int32_t main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    //int t; cin >> t;
-    int t = 1;
+    int t; cin >> t;
+    // int t = 1;
     while(t--) solve();
 
     return 0;
