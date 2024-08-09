5d2418bc9f06f4c20d71f40ffd8d0ff7e71001dc
tanvir <shkhtanvirahmed@gmail.com>
2024-08-09 12:00:06 +0600

    Automated commit message

---
+++ cf/all 900/A_DualTrigger.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define input freopen("in.txt", "r", stdin)
+#define pr(x) cout << #x << " = " << x << endl
+#define prt(x) cout << x << endl
+#define endl "\n"
+#define hi cout << "hi" << endl
+//#define int long long
+//#define int unsigned long long
+
+void solve(){
+    int n; cin >> n;
+    string str; cin >> str;
+
+    int cnt = count(str.begin(), str.end(), '1');
+    pr(cnt);
+    if(n == 2 && cnt != 2){
+        prt("NO");
+        return;
+    }
+    if(cnt%2 == 0) prt("YES");
+    else prt("NO");
+}
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t; cin >> t;
+    // int t = 1;
+    while(t--) solve();
+
+    return 0;
+}
