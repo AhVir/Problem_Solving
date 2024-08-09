f41eb5b7ab0b6a93c7b86f31d61804d89cf93e2b
tanvir <shkhtanvirahmed@gmail.com>
2024-08-09 23:00:11 +0600

    Automated commit message

---
+++ CPS/contests/weekly_25/O.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define input freopen("in.txt", "r", stdin)
+#define pr(x) cout << #x << " = " << x << endl
+#define prt(x) cout << x << endl
+#define endl "\n"
+#define hi cout << "hi" << endl
+#define int long long
+//#define int unsigned long long
+
+void solve(){
+    int n, m; cin >> n >> m;
+    vector<string> pictures;
+
+    for(int i=0; i<n; i++){
+        string temp = "";
+        for(int j=0; j<m; j++){
+            char t; cin >> t;
+            temp.push_back(t);
+        }
+        pictures.push_back(temp);
+    }
+    // prt("Printing: ");
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++) cout << pictures[i][j];
+        cout << endl;
+    }
+
+    //finding maxRow count:
+    int rowCount = 0;
+    int maxRowIdx = -1;
+    for(int i=0; i<n; i++){
+        int tempCnt = 0;
+        for(int j=0; j<m; j++){
+            if(pictures[i][j] == '*') tempCnt++;
+        }
+        rowCount = max(rowCount, tempCnt);
+        if(tempCnt > rowCount){
+            rowCount = tempCnt;
+            maxRowI
+        }
+    }
+
+    int colCount = 0;
+    for(int i=0; i<m; i++){
+        int tempCnt = 0;
+        for(int j=0; j<n; j++){
+            if(pictures[j][i] == '*') tempCnt++;
+        }
+        colCount = max(colCount, tempCnt);
+    }
+
+    // pr(rowCount);
+    // pr(colCount);
+    if(colCount == n && rowCount == m) prt(0);
+    else{
+        int neededMin = (n-colCount) + (m-rowCount);
+        pr(neededMin);
+    }
+}
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t; cin >> t;
+    // int t = 1;
+    while(t--) solve();
+
+    return 0;
+}
