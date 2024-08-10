144157ae54e812e35fcc6febbc5cb575779bbc56
tanvir <shkhtanvirahmed@gmail.com>
2024-08-10 21:00:05 +0600

    Automated commit message

---
+++ cf/0_Contest/div2_965/B.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define input freopen("in.txt", "r", stdin)
+#define pr(x) cout << #x << " = " << x << endl
+#define prt(x) cout << x << endl
+#define endl "\n"
+#define hi cout << "hi" << endl
+//#define int long long
+//#define int unsigned long long
+
+void solve(){
+
+}
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    //int t; cin >> t;
+    int t = 1;
+    while(t--) solve();
+
+    return 0;
+}
