2f68279f1fd49d382a013b7e4f364a66524e9ef6
tanvir <shkhtanvirahmed@gmail.com>
2024-08-15 06:00:05 +0600

    Automated commit message

--- someTopics/regex/basicRegex.cpp
+++ someTopics/regex/basicRegex.cpp
@@ -2,7 +2,41 @@
 using namespace std;
 
 int main(){
-    string text = ""
+    string text = "My name is tanvir, you are tanvir, i'm tanvir, we all are tanvir, tanvir tanvir tanvir";
+    regex rg("tanvir");
+
+    //searching on a string
+    if(regex_search(text, rg)) cout << "Test contains the word 'tanvir'" << endl;
+
+    //finding number of occurrence 
+    auto tanvir_begins = sregex_iterator(text.begin(), text.end(), rg);
+    auto tanvir_ends = sregex_iterator();
+
+
+    cout << "#occurrence = " << distance(tanvir_begins, tanvir_ends) << "." << endl;
+
+    //printing all the occurrences
+    for(auto i = tanvir_begins; i != tanvir_ends; i++){
+        smatch match = *i;
+        string matchedString = match.str();
+        cout << matchedString << " ";
+    }
+    cout << endl;
+
+
+    //printing without the target words
+    // -1 -> prints without the matched words, works as a splitters
+    //  0 -> prints the matched words
+    copy(sregex_token_iterator(text.begin(), text.end(), rg, 0), sregex_token_iterator(), ostream_iterator<string>(cout, " "));
+    cout << endl;
+
+
+    // string s = "this subject has a submarine as a subsequences";
+    // regex rgx("sub");
+    // cout << "Entire matches: " << endl;
+    // regex_token_iterator<string::iterator> strItr(s.begin(), s.end(), rgx);
+    // while(strItr != rend) cout << *strItr++ << ", "; 
+    // cout << endl;
 
     return 0;
 }
---
+++ someTopics/regex/syntaxTesting.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define input freopen("in.txt", "r", stdin)
+#define pr(x) cout << #x << " = " << x << endl
+#define prt(x) cout << x << endl
+#define endl "\n"
+#define hi cout << "hi" << endl
+//#define int long long
+//#define int unsigned long long
+
+void solve(){
+    string text = "my name is 007";
+    regex rgx("\\d{4}");
+
+    if(regex_search(text, rgx)) cout << "Valid text, PASS!" << endl;
+    else cout << "Invalid Test" << endl;
+}
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    //int t; cin >> t;
+    int t = 1;
+    while(t--) solve();
+
+    return 0;
+}
