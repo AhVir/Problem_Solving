3d16377fcae618eb8917778736f2ef02faefa8fe
tanvir <shkhtanvirahmed@gmail.com>
2024-08-15 23:00:05 +0600

    Automated commit message

--- cf/all 900/B_Decoding.cpp
+++ cf/all 900/B_Decoding.cpp
@@ -14,39 +14,49 @@ void solve(){
     string str; cin >> str;
 
     vector<char> ans(n, ' ');
-    int median = 0;
-    int temp = 0;
-    if(n%2 == 0) median = (n/2)-1, temp = n-2, ans[n-1] = str[n-1];
-    else median = (n/2), temp = n-1;
-    ans[median] = str[0];
-    // pr(str);
-    // for(char X:ans) cout << X;
-    int cnt = 1, left = 1, right = 1;
-    while(temp--){
-        if(cnt%2 != 0){
-            // pr(left);
-            int idx = median - left;
-            // prt("left:");
-            // pr(idx);
-            left++;
-            ans[idx] = str[cnt];
-            // for(char X:ans) cout << X;
-            // cout << endl;
+    if(n%2 != 0){
+         int median = (n/2), temp = n-1;
+        ans[median] = str[0];
+        int cnt = 1, left = 1, right = 1;
+        while(temp--){
+            if(cnt%2 != 0){
+                int idx = median - left;
+                left++;
+                ans[idx] = str[cnt];
+            }
+            else{
+                int idx = median + right;
+                right++;
+                ans[idx] = str[cnt];
+            }
+            cnt++;
         }
-        else{
-            int idx = median + right;
-            // prt("right:");
-            // pr(idx);
-            right++;
-            ans[idx] = str[cnt];
-            // for(char X:ans) cout << X;
-            // cout << endl;
-        }
-        cnt++;
-    }
 
-    for(char X:ans) cout << X;
-    cout << endl;
+        for(char X:ans) cout << X;
+        cout << endl;
+    }
+    else{
+        int median = (n/2) - 1;
+        ans[median] = str[0];
+        int cnt = 1, left = 1, right = 1;
+        int temp = n-1;
+        while(temp--){
+            if(cnt%2 == 0){
+                int idx = median - left;
+                left++;
+                ans[idx] = str[cnt];
+            }
+            else{
+                int idx = median + right;
+                right++;
+                ans[idx] = str[cnt];
+            }
+            cnt++;
+        }
+
+        for(char X:ans) cout << X;
+        cout << endl;
+    }
 }
 
 int32_t main(){
