2cbe3896dfb0908ed8014c8af6890194e7748149
tanvir <shkhtanvirahmed@gmail.com>
2024-08-19 23:00:05 +0600

    Automated commit message

--- cf/all 800/A_PrimaryTask.cpp
+++ cf/all 800/A_PrimaryTask.cpp
@@ -13,16 +13,47 @@ using namespace std;
 
 void solve(){
     string num; cin >> num;
-    pr(num[0]);
-    pr(num[1]);
+    int n = num.size();
+    // pr(num[0]);
+    // pr(num[1]);
+
+    if(n == 1 || n == 2){
+        prt("No");
+        return;
+    }
+
+    if(num[0] != '1' || num[1] != '0'){
+        prt("NO");
+        return;
+    }
+
+    char firstPower = 0;
+    int firstPowerIdx = -1;
+    if(num[2] == '0'){
+        prt("NO");
+        return;
+    }
+
+    if(num[2] >= '2'){
+        prt("YES");
+        return;
+    }
+    else if(num[2] == '1' && (n-1) >= 3){
+        prt("YES");
+        return;
+    }
+    else{
+        prt("NO");
+        return;
+    }
 }
 
 int32_t main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    //int t; cin >> t;
-    int t = 1;
+    int t; cin >> t;
+    // int t = 1;
     while(t--) solve();
 
     return 0;
