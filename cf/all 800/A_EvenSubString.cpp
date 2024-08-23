34535ea3b1411564b74dc1cfe4d8a5928ccb9e38
tanvir <shkhtanvirahmed@gmail.com>
2024-08-23 21:00:06 +0600

    Automated commit message

---
+++ cf/all 800/A_EvenSubString.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define input freopen("in.txt", "r", stdin)
+#define debugIn freopen("out.txt", "r", stdin)
+#define pr(x) cout << #x << " = " << x << endl
+#define prt(x) cout << x << endl
+#define endl "\n"
+#define hi cout << "hi" << endl
+#define br cout << endl
+//#define int long long
+//#define int unsigned long long
+
+void solve(){
+    int n; cin >> n;
+    string num; cin >> num;
+
+    int evenSubStringCnt = 0;
+    
+    for(int i=0; i<n; i++){
+        int number = num[i] - '0';
+        if(number%2 == 0) evenSubStringCnt++;
+    }
+}
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    //int t; cin >> t;
+    int t = 1;
+    while(t--) solve();
+
+    return 0;
+}
