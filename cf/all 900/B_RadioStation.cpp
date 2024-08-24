657d3013aeeba8829bd0b6b22adcfd77e0991b33
tanvir <shkhtanvirahmed@gmail.com>
2024-08-24 10:00:05 +0600

    Automated commit message

--- cf/all 900/B_RadioStation.cpp
+++ cf/all 900/B_RadioStation.cpp
@@ -12,7 +12,33 @@ using namespace std;
 //#define int unsigned long long
 
 void solve(){
+    int n, m; cin >> n >> m;
+    vector<string> mainNames(n), mainIps(n);
+    for(int i=0; i<n; i++){
+        string t1, t2; cin >> t1 >> t2;
+        mainNames[i] = t1, mainIps[i] = t2;
+    }
 
+    vector<string> configureNames(m), configureIps(m);
+    for(int i=0; i<m; i++){
+        string t1, t2; cin >> t1 >> t2;
+        configureNames[i] = t1, configureIps[i] = t2;
+    }
+
+    for(int i=0; i<m; i++){
+        string targetIP = configureIps[i];
+        targetIP.erase(targetIP.length()-1, 1);
+
+        int idx = -1;
+        for(int i=0; i<n; i++){
+            if(mainIps[i] == targetIP){
+                idx = i;
+                break;
+            }
+        }
+
+        cout << configureNames[i] << " " << configureIps[i] << " #" << ""
+    }
 }
 
 int32_t main(){
