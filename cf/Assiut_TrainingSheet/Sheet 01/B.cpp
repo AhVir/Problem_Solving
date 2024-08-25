8da4deb007dd860a31ee1c5eec83cb76e6e4fa8b
tanvir <shkhtanvirahmed@gmail.com>
2024-08-26 00:41:50 +0600

    solving Assiut Sheet 01

---
+++ cf/Assiut_TrainingSheet/Sheet 01/B.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define input freopen("in.txt", "r", stdin)
+#define debugIn freopen("out.txt", "r", stdin)
+#define pr(x) cout << #x << " = " << x << endl
+#define prt(x) cout << x << endl
+#define endl "\n"
+#define hi cout << "hi" << endl
+#define br cout << endl
+//#define int long long
+//#define int unsigned long long
+
+void solve(){
+    for(int i=1; i<=5; i++){
+        string s; cin >> s;
+        cout << s << endl;;
+    }
+}
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    //int t; cin >> t;
+    int t = 1;
+    while(t--) solve();
+
+    return 0;
+}
---
+++ cf/Assiut_TrainingSheet/Sheet 01/E.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define input freopen("in.txt", "r", stdin)
+#define debugIn freopen("out.txt", "r", stdin)
+#define pr(x) cout << #x << " = " << x << endl
+#define prt(x) cout << x << endl
+#define endl "\n"
+#define hi cout << "hi" << endl
+#define br cout << endl
+//#define int long long
+//#define int unsigned long long
+
+void solve(){
+    double pi = 3.141592653;
+    float num; cin >> num;
+    double cal = num * pi * num;
+
+    // printf("%.9f\n", cal);
+
+    cout << fixed << setprecision(9) << cal << endl;
+}
+
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    //int t; cin >> t;
+    int t = 1;
+    while(t--) solve();
+
+    return 0;
+}
