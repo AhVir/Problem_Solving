ba030a8120c8b9b7471038c7755412d7d91d3e0f
tanvir <shkhtanvirahmed@gmail.com>
2024-08-26 02:11:39 +0600

    .

---
+++ cf/Assiut_TrainingSheet/Sheet 01/F.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define input freopen("in.txt", "r", stdin)
+#define debugIn freopen("out.txt", "r", stdin)
+#define pr(x) cout << #x << " = " << x << endl
+#define prt(x) cout << x << endl
+#define endl "\n"
+#define hi cout << "hi" << endl
+#define br cout << endl
+#define int long long
+//#define int unsigned long long
+
+void solve(){
+    int n, m; cin >> n >> m;
+
+    cout << (n%10)+(m%10) << endl;
+}
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    //int t; cin >> t;
+    int t = 1;
+    while(t--) solve();
+
+    return 0;
+}
---
+++ cf/Assiut_TrainingSheet/Sheet 01/I.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define input freopen("in.txt", "r", stdin)
+#define debugIn freopen("out.txt", "r", stdin)
+#define pr(x) cout << #x << " = " << x << endl
+#define prt(x) cout << x << endl
+#define endl "\n"
+#define hi cout << "hi" << endl
+#define br cout << endl
+//#define int long long
+//#define int unsigned long long
+
+void solve(){
+    int a, b; cin >> a >> b;
+    if(a >= b) prt("Yes");
+    else prt("No");
+}
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    //int t; cin >> t;
+    int t = 1;
+    while(t--) solve();
+
+    return 0;
+}
