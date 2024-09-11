87819ed46657b2e2ca3f0e4c33b50c87c7d5d34e
tanvir <shkhtanvirahmed@gmail.com>
2024-09-12 00:01:11 +0600

    .

---
+++ 0_cf/Assiut_TrainingSheet/Sheet 02/N_Numbers_Histogram.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define input freopen("in.txt", "r", stdin)
+#define debugIn freopen("out.txt", "r", stdin)
+#define pr(x) cout << #x << " = " << x << endl
+#define prt(x) cout << x << endl
+#define endl "\n"
+#define hi cout << "hi" << endl
+#define br cout << endl
+//#define int long long
+//#define int unsigned long long
+
+void solve(){
+    char symbol; cin >> symbol;
+
+    int n; cin >> n;
+    for(int i=0; i<n; i++){
+        int num; cin >> num;
+        for(int j=1; j<=num; j++) cout << symbol;
+        cout << endl;
+    }
+}
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    //int t; cin >> t;
+    int t = 1;
+    while(t--) solve();
+
+    return 0;
+}
---
+++ 0_cf/Assiut_TrainingSheet/Sheet 02/O_Pyramid.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define input freopen("in.txt", "r", stdin)
+#define debugIn freopen("out.txt", "r", stdin)
+#define pr(x) cout << #x << " = " << x << endl
+#define prt(x) cout << x << endl
+#define endl "\n"
+#define hi cout << "hi" << endl
+#define br cout << endl
+//#define int long long
+//#define int unsigned long long
+
+void solve(){
+    int n; cin >> n;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++) cout << '*';
+        cout << endl;
+    }
+}
+
+int32_t main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    //int t; cin >> t;
+    int t = 1;
+    while(t--) solve();
+
+    return 0;
+}
